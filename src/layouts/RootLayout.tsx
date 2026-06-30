b1b67bc81c52cef7d088a3574b442d24509c31ab
Muigaidennis <denniskinuthiamuigai@gmail.com>
2026-06-30 18:05:05 +0300

    Add RootLayout component with Outlet

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,1 +1,11 @@
+import { Outlet } from 'react-router-dom';
 
+const RootLayout = () => {
+  return (
+    <div className="min-h-screen">
+      <Outlet />
+    </div>
+  );
+};
+
+export default RootLayout;
